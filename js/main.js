57153b07c151ac7fff5d5784b43d589c5b99c604
benj-san <benjamin.beugnet@wildcodeschool.fr>
2019-09-21 12:31:51 +0200

    refactor and optimize fading + automatisation

--- js/main.js
+++ js/main.js
@@ -1,48 +1,74 @@
 //I put all my pictures into an array
-let allMyPics = Array(document.getElementsByClassName('sliderPic'));
+let allMyPics = document.getElementsByClassName('sliderPic');
 
 // I generate the buttons triggering actions in order to change the picture on click
 document.getElementById('nextSlide').addEventListener('click', function(){
 
     //I search for the focused picture and parse its value in integer
     let myFocusedPic = parseInt(document.getElementById('focused').getAttribute('alt').substr(-1, 1));
+
+    //i remove the focus on the current picture
     document.getElementById('focused').removeAttribute('id');
 
-
     // I check if my current pic is not the last of the slider
-    if(myFocusedPic < allMyPics[0].length){
+    if(myFocusedPic < allMyPics.length){
 
         //I change the picture focused finding the next one into the array
-        allMyPics[0][myFocusedPic].setAttribute('id', 'focused');
+        allMyPics[myFocusedPic].setAttribute('id', 'focused');
 
     }
 
     //else I go back to the first one
     else{
-        allMyPics[0][0].setAttribute('id', 'focused');
+        allMyPics[0].setAttribute('id', 'focused');
     }
 
 });
 
-// I generate the buttons triggering actions in order to change the picture on click
 document.getElementById('prevSlide').addEventListener('click', function(){
 
-    //I search for the focused picture and parse its value in integer
     let myFocusedPic = parseInt(document.getElementById('focused').getAttribute('alt').substr(-1, 1));
     document.getElementById('focused').removeAttribute('id');
 
-
-    // I check if my current pic is not the last of the slider
     if(myFocusedPic > 1){
-
-        //I change the picture focused finding the next one into the array
-        allMyPics[0][myFocusedPic -2].setAttribute('id', 'focused');
-
+        allMyPics[myFocusedPic -2].setAttribute('id', 'focused');
     }
-
-    //else I go back to the first one
     else{
-        allMyPics[0][allMyPics[0].length -1].setAttribute('id', 'focused');
+        allMyPics[allMyPics.length -1].setAttribute('id', 'focused');
     }
 
+});
+
+
+let myInterval;
+
+//Put the setInterval into a castable variable, encapsulate it into a function
+function sliderAutoRun(){
+    myInterval = setInterval(changePic, 7000);
+};
+
+//Set the auto rotation of the slider
+function changePic(){
+    let myFocusedPic = parseInt(document.getElementById('focused').getAttribute('alt').substr(-1, 1));
+    document.getElementById('focused').removeAttribute('id');
+
+    if(myFocusedPic < allMyPics.length){
+        allMyPics[myFocusedPic].setAttribute('id', 'focused');
+    }
+    else{
+        allMyPics[0].setAttribute('id', 'focused');
+    }
+}
+
+//Launch the autoRun
+sliderAutoRun();
+
+//ResetAutorun when needed -- queryselectorall is needed because tagname collection return a html collection, unmatching the foreach
+let myButtons = document.getElementById('container').querySelectorAll('button');
+myButtons.forEach(e => {
+    e.addEventListener('click', function(){
+        console.log('you pulled the trigger of my ...');
+        clearInterval(myInterval);
+        sliderAutoRun();
+    })
 });
